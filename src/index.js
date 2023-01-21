41c9085b8ee0a21047b7a7ab017311c676316f82
TheLokin <diego.ramil.lopez@gmail.com>
2023-01-21 16:38:13 +0100

    Add hono

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npx wrangler dev src/index.js` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npx wrangler publish src/index.js --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from "hono";
 
-export default {
-  async fetch(request, env, ctx) {
-    return new Response("Hello World!");
-  },
-};
+const app = new Hono();
+
+app.get("/", (ctx) => {
+  ctx.json({ message: "Hello World!" });
+});
