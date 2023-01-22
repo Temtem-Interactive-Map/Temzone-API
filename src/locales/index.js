87469b1f96d93ecb86a630ad49350d1ccf5320df
TheLokin <diego.ramil.lopez@gmail.com>
2023-01-22 23:57:11 +0100

    Fix exception when key is not found

--- src/locales/index.js
+++ src/locales/index.js
@@ -1,7 +1,7 @@
 import en from "locales/en.json";
 
-export function t(keys, params) {
-  const key = keys.split(".").reduce((o, i) => o[i], en);
+export function t(keys, ...params) {
+  const key = keys.split(".").reduce((o, i) => o[i] ?? keys, en);
 
   return Object.keys(params).reduce(
     (o, i) => o.replace("@" + i, params[i]),
