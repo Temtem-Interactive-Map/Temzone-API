40cd00f9d8d3e6cd60dfc7c7fb5522f5618a3aa2
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-01 01:52:17 +0100

    Update tests

--- src/api/index.test.js
+++ src/api/index.test.js
@@ -1,5 +1,23 @@
+import * as dotenv from "dotenv";
 import { afterAll, beforeAll, describe, expect, it } from "vitest";
 import { unstable_dev as unstableDev } from "wrangler";
+
+async function getFirebaseToken(email, password) {
+  return fetch(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+      process.env.FIREBASE_API_KEY,
+    {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true,
+      }),
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => data.idToken);
+}
 
 function checkProperties(subject, schema) {
   schema.forEach(({ name, type }) => {
@@ -19,10 +37,30 @@ const errorProperties = [
   { name: "message", type: "string" },
 ];
 
-describe("Testing '/markers' route", () => {
+const temtemMarkerProperties = [
+  { name: "id", type: "number" },
+  { name: "type", type: "string" },
+  { name: "title", type: "string" },
+  { name: "subtitle", type: "string" },
+  { name: "coordinates", type: "object" },
+];
+
+describe("Testing routes", () => {
+  let userToken;
+  let adminToken;
   let worker;
 
   beforeAll(async () => {
+    dotenv.config();
+
+    userToken = await getFirebaseToken(
+      process.env.FIREBASE_USER_EMAIL,
+      process.env.FIREBASE_USER_PASSWORD
+    );
+    adminToken = await getFirebaseToken(
+      process.env.FIREBASE_ADMIN_EMAIL,
+      process.env.FIREBASE_ADMIN_PASSWORD
+    );
     worker = await unstableDev("src/api/index.js", {
       experimental: { disableExperimentalWarning: true },
     });
@@ -32,40 +70,56 @@ describe("Testing '/markers' route", () => {
     await worker.stop();
   });
 
-  const markerProperties = [
-    { name: "id", type: "number" },
-    { name: "type", type: "string" },
-    { name: "title", type: "string" },
-    { name: "subtitle", type: "string" },
-    { name: "coordinates", type: "object" },
-  ];
-
-  const coordinatesProperties = [
-    { name: "x", type: "number" },
-    { name: "y", type: "number" },
-  ];
-
-  it("should", async () => {
-    const response = await worker.fetch("/markers", {
+  it("route '/' should return 401 Unauthorized", async () => {
+    const response = await worker.fetch("/", {
       method: "GET",
-      headers: {
-        Authorization: "Bearer ",
-      },
     });
 
     expect(response).toBeDefined();
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(401);
 
     const data = await response.json();
 
     checkProperties(data, errorProperties);
   });
 
-  it("should return marker props", async () => {
-    const response = await worker.fetch("/markers", {
+  it("route '/' should return 404 Not Found with user token", async () => {
+    const response = await worker.fetch("/", {
       method: "GET",
       headers: {
-        Authorization: "Bearer ",
+        Authorization: "Bearer " + userToken,
+      },
+    });
+
+    expect(response).toBeDefined();
+    expect(response.status).toBe(404);
+
+    const data = await response.json();
+
+    checkProperties(data, errorProperties);
+  });
+
+  it("route '/' should return 404 Not Found with admin token", async () => {
+    const response = await worker.fetch("/", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + adminToken,
+      },
+    });
+
+    expect(response).toBeDefined();
+    expect(response.status).toBe(404);
+
+    const data = await response.json();
+
+    checkProperties(data, errorProperties);
+  });
+
+  it("route '/markers' should return 200 Ok with temtem marker props", async () => {
+    const response = await worker.fetch("/markers?type=temtem", {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + userToken,
       },
     });
 
@@ -74,6 +128,8 @@ describe("Testing '/markers' route", () => {
 
     const data = await response.json();
 
-    // data.forEach((endpoint) => checkProperties(endpoint, markerProperties));
+    data.items.forEach((marker) => {
+      checkProperties(marker, temtemMarkerProperties);
+    });
   });
 });
