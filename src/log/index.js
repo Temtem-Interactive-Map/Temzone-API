781086c7a49027ed51cfad22e5662b023461431d
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-02 19:37:14 +0100

    Update logs

--- src/log/index.js
+++ src/log/index.js
@@ -7,11 +7,18 @@ const symbol = {
   error: pc.red("✖"),
 };
 
-export const logInfo = (...args) =>
+export function logInfo(...args) {
   console.log(symbol.info + " " + pc.blue(...args));
-export const logError = (...args) =>
+}
+
+export function logError(...args) {
   console.log(symbol.error + " " + pc.red(...args));
-export const logSuccess = (...args) =>
+}
+
+export function logSuccess(...args) {
   console.log(symbol.success + " " + pc.green(...args));
-export const logWarning = (...args) =>
+}
+
+export function logWarning(...args) {
   console.log(symbol.warning + " " + pc.yellow(...args));
+}
