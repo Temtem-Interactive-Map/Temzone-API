3ef9433ab8b588d064e5ed73f361f4814be9fa0a
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-07 02:56:02 +0100

    Update db path

--- scraper/db/index.js
+++ scraper/db/index.js
@@ -1,11 +1,11 @@
 import { writeFile } from "node:fs/promises";
 import path from "node:path";
 
-const DB_PATH = path.join(process.cwd(), "./db/");
+const DB_PATH = path.join(process.cwd(), "scraper", "db");
 
 export function writeDBFile(dbName, data) {
   return writeFile(
-    DB_PATH + "/" + dbName + ".json",
+    path.join(DB_PATH, dbName + ".json"),
     JSON.stringify(data, null, 2),
     "utf-8"
   );
