c6230be3cf166787a1cbbb3817759dd685507318
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-07 02:55:23 +0100

    Update main scraper

--- scraper/index.js
+++ scraper/index.js
@@ -1,5 +1,58 @@
-import { logInfo } from "./log/index.js";
-import { SCRAPINGS, scrapeAndSave } from "./utils/index.js";
+import { writeDBFile } from "./db/index.js";
+import { logError, logInfo, logSuccess } from "./log/index.js";
+import { getTraits } from "./traits.js";
+import { getTypes } from "./types.js";
+import { scrape } from "./utils/index.js";
+
+const SCRAPINGS = {
+  types: {
+    urls: ["https://temtem.wiki.gg/wiki/Temtem_types"],
+    scraper: getTypes,
+  },
+  traits: {
+    urls: ["https://temtem.wiki.gg/wiki/Traits"],
+    scraper: getTraits,
+  },
+  temtem: {
+    urls: ["https://temtem.wiki.gg/wiki/Temtem_(creatures)"],
+    scraper: null,
+  },
+  saipark: {
+    urls: ["https://temtem.wiki.gg/wiki/Saipark"],
+    scraper: null,
+  },
+  landmarks: {
+    urls: [],
+    scraper: null,
+  },
+};
+
+async function scrapeAndSave(name) {
+  const start = performance.now();
+
+  try {
+    const { urls, scraper } = SCRAPINGS[name];
+
+    logInfo("Scraping [" + name + "]...");
+    for (const url of urls) {
+      const $ = await scrape(url);
+      const content = await scraper($);
+
+      logSuccess("[" + name + "] scraped successfully");
+
+      logInfo("Writing [" + name + "] to database...");
+      await writeDBFile(name, content);
+      logSuccess("[" + name + "] written successfully");
+    }
+  } catch (error) {
+    logError("Error scraping [" + name + "]");
+    logError(error);
+  } finally {
+    const end = performance.now();
+    const time = Math.round((end - start) / 10) / 100;
+    logInfo("[" + name + "] scraped in " + time + " seconds");
+  }
+}
 
 const scrapeParameter = process.argv.at(-1);
 
