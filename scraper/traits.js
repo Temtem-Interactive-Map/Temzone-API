7fa6c0b0f0827f89e6191f48d718eb28e02ba4c6
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-08 16:22:43 +0100

    Update traits scraper

--- scraper/traits.js
+++ scraper/traits.js
@@ -1,20 +1,32 @@
-import { scrapeTable } from "./utils/index.js";
+import { cleanText } from "./utils/index.js";
 
 const TRAIT_SELECTORS = {
   name: {
     selector: "td:nth-child(1)",
-    typeOf: "string",
   },
   description: {
     selector: "td:nth-child(2)",
-    typeOf: "string",
   },
 };
 
-export function getTraits($) {
-  return scrapeTable(
-    $,
-    "table.wikitable > tbody > tr:not(:first-child)",
-    TRAIT_SELECTORS
-  );
+export async function getTraits($) {
+  const traits = {};
+  const traitSelectorEntries = Object.entries(TRAIT_SELECTORS);
+  const $rows = $("table.wikitable > tbody > tr:not(:first-child)");
+
+  $rows.each((_, el) => {
+    const $el = $(el);
+    const traitEntries = traitSelectorEntries.map(([key, { selector }]) => {
+      const rawValue = $el.find(selector).text();
+      const value = cleanText(rawValue);
+
+      return [key, value];
+    });
+
+    const trait = Object.fromEntries(traitEntries);
+
+    traits[trait.name] = trait;
+  });
+
+  return traits;
 }
