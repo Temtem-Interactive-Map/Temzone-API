8a17d4703670c978f6de380de7d78845ef4775e3
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-08 16:23:14 +0100

    Update utils

--- scraper/utils/index.js
+++ scraper/utils/index.js
@@ -7,29 +7,6 @@ export async function scrape(url) {
   return load(html);
 }
 
-export async function scrapeTable($, element, selectors) {
-  const table = [];
-  const selectorEntries = Object.entries(selectors);
-  const $rows = $(element);
-
-  $rows.each((_, el) => {
-    const $el = $(el);
-    const entries = selectorEntries.map(([key, { selector, typeOf }]) => {
-      const rawValue = $el.find(selector).text();
-      const cleanedValue = cleanText(rawValue);
-      const value = typeOf === "number" ? Number(cleanedValue) : cleanedValue;
-
-      return [key, value];
-    });
-
-    const content = Object.fromEntries(entries);
-
-    table.push(content);
-  });
-
-  return table;
-}
-
 export function cleanText(text) {
   return text
     .replace(/\t|\n|\u200a/g, "")
