be8b9dafcdcd98a5ac842cc31b7476744d47f977
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-13 06:04:16 +0100

    Add db reader

--- scraper/db/index.js
+++ scraper/db/index.js
@@ -1,4 +1,4 @@
-import { writeFile } from "node:fs/promises";
+import { writeFile, readFile } from "node:fs/promises";
 import path from "node:path";
 
 const DB_PATH = path.join(process.cwd(), "scraper", "db");
@@ -8,5 +8,11 @@ export function writeDBFile(dbName, data) {
     path.join(DB_PATH, dbName + ".json"),
     JSON.stringify(data, null, 2),
     "utf-8"
+  );
+}
+
+export function readDBFile(dbName) {
+  return readFile(path.join(DB_PATH, dbName + ".json"), "utf-8").then(
+    JSON.parse
   );
 }
