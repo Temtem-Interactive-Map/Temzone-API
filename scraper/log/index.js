c173446fb549bf3a35a3ba0f412a443b5a29e297
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-13 04:30:17 +0100

    Update scraper logs

--- scraper/log/index.js
+++ scraper/log/index.js
@@ -3,7 +3,6 @@ import pc from "picocolors";
 const symbol = {
   info: pc.blue("ℹ"),
   success: pc.green("✔"),
-  warning: pc.yellow("⚠"),
   error: pc.red("✖"),
 };
 
@@ -11,14 +10,10 @@ export function logInfo(...args) {
   console.log(symbol.info + " " + pc.blue(...args));
 }
 
-export function logError(...args) {
-  console.log(symbol.error + " " + pc.red(...args));
-}
-
 export function logSuccess(...args) {
   console.log(symbol.success + " " + pc.green(...args));
 }
 
-export function logWarning(...args) {
-  console.log(symbol.warning + " " + pc.yellow(...args));
+export function logError(...args) {
+  console.log(symbol.error + " " + pc.red(...args));
 }
