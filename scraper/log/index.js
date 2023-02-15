ea8ef0310a5f7633579122cfad0966cb3985e72f
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-15 18:18:59 +0100

    Update scraper log

--- scraper/log/index.js
+++ scraper/log/index.js
@@ -2,12 +2,17 @@ import pc from "picocolors";
 
 const symbol = {
   info: pc.blue("ℹ"),
+  warning: pc.yellow("⚠"),
   success: pc.green("✔"),
   error: pc.red("✖"),
 };
 
 export function logInfo(...args) {
   console.log(symbol.info + " " + pc.blue(...args));
+}
+
+export function logWarning(...args) {
+  console.log(symbol.warning + " " + pc.yellow(...args));
 }
 
 export function logSuccess(...args) {
