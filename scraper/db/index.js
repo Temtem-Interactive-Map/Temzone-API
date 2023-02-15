d997120ddeb9f61ab52ae4d6b947ff6880688870
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-15 18:20:23 +0100

    Update db scraper

--- scraper/db/index.js
+++ scraper/db/index.js
@@ -1,30 +1,25 @@
-import download from "image-downloader";
-import { readFile, writeFile } from "node:fs/promises";
-import path from "node:path";
+import { readdir, unlink, writeFile } from "node:fs/promises";
+import { join } from "node:path";
 
-const DB_PATH = path.join(process.cwd(), "scraper", "db");
+export async function writeDBFile(fileName, data) {
+  const path = join(process.cwd(), "scraper", "db", fileName + ".json");
 
-export function writeDBFile(dbName, data) {
-  return writeFile(
-    path.join(DB_PATH, dbName + ".json"),
-    JSON.stringify(data, null, 2),
-    "utf-8"
-  );
+  await writeFile(path, JSON.stringify(data, null, 2));
 }
 
-export function readDBFile(dbName) {
-  return readFile(path.join(DB_PATH, dbName + ".json"), "utf-8").then(
-    JSON.parse
-  );
+export async function removeDBContent(directory) {
+  const path = join(process.cwd(), "assets", "static", directory);
+
+  for (const file of await readdir(path)) {
+    await unlink(join(path, file));
+  }
 }
 
-export function writeDBImage(dbName, url) {
-  const dest = path.join(DB_PATH, "assets", dbName + ".png");
+export async function writeDBImage(fileName, url) {
+  const path = join(process.cwd(), "assets", "static", fileName);
+  const response = await fetch(url);
+  const arrayBuffer = await response.arrayBuffer();
+  const buffer = Buffer.from(arrayBuffer);
 
-  download.image({
-    url,
-    dest,
-  });
-
-  return dest;
+  await writeFile(path, buffer);
 }
