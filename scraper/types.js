8042b680e0a5216b295d59d45dc9f4a0ed1edc5d
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-15 18:20:09 +0100

    Refactor types scraper

--- scraper/types.js
+++ scraper/types.js
@@ -1,35 +1,53 @@
-import { writeDBImage } from "./db/index.js";
-import { cleanText, scrape } from "./utils/index.js";
+import { join } from "node:path";
+import { removeDBContent, writeDBFile, writeDBImage } from "./db/index.js";
+import { logInfo, logSuccess, logWarning } from "./log/index.js";
+import { cleanText, getUrlExtension, scrape } from "./utils/index.js";
 
-export async function getAllTypes() {
-  const types = {};
-  const $ = await scrape("https://temtem.wiki.gg/wiki/Temtem_types");
+export class TypesDB {
+  static async scrape() {
+    if (this.types) return logWarning("[types] already scraped");
 
-  $("ul:nth-child(6) > li > a:nth-child(1)")
-    .toArray()
-    .forEach((el) => {
+    logWarning("Removing [types] database...");
+    await removeDBContent("types");
+    logSuccess("[types] database removed successfully");
+
+    logInfo("Scraping [types]...");
+    this.types = {};
+
+    const $ = await scrape("https://temtem.wiki.gg/wiki/Temtem_types");
+
+    for (const el of $("ul:nth-child(6) > li > a:nth-child(1)")) {
       const $el = $(el);
       const rawName = $el.attr("title");
       const name = cleanText(rawName);
-      const rawUrl = $(el).find("img").attr("src");
+      const rawUrl = $el.find("img").attr("src");
       const url = cleanText(rawUrl);
+      const extension = getUrlExtension(url);
+      const fileName = name.replace(" ", "-").toLowerCase() + "." + extension;
 
-      const image = writeDBImage(
-        name.replace(" ", "_").toLowerCase(),
+      logInfo("- Writing [" + fileName + "] to database...");
+      await writeDBImage(
+        join("types", fileName),
         "https://temtem.wiki.gg/" + url
       );
+      logSuccess("- [" + fileName + "] written successfully");
 
-      types[name] = {
+      this.types[name] = {
         name,
-        image,
+        image: "static/types/" + fileName,
       };
-    });
+    }
 
-  return types;
-}
+    logSuccess("[types] scraped successfully");
+  }
 
-const types = Object.freeze(await getAllTypes());
+  static async write() {
+    logInfo("Writing [types] to database...");
+    await writeDBFile("types", this.types);
+    logSuccess("[types] written successfully");
+  }
 
-export function findType(name) {
-  return types[name];
+  static find(name) {
+    return this.types[name];
+  }
 }
