ffebfe675a975fb0d8e7d6c23912c9ad96163991
TheLokin <diego.ramil.lopez@gmail.com>
2023-02-18 16:51:01 +0100

    Refactor main scraper

--- scraper/index.js
+++ scraper/index.js
@@ -12,39 +12,37 @@ const SCRAPERS = {
   temtem: TemtemDB,
   spawns: SpawnsDB,
   saipark: SaiparkDB,
-  landmarks: null,
 };
 
 async function scrapeAndSave(name) {
-  const start = performance.now();
+  const scraper = SCRAPERS[name];
+  const content = await scraper.scrape();
 
-  try {
-    const scraper = SCRAPERS[name];
-    const content = await scraper.scrape();
-
-    logInfo("Writing [" + name + "] to database...");
-    await writeDBFile(name, content);
-    logSuccess("[" + name + "] written successfully");
-  } catch (error) {
-    logError("Error scraping [" + name + "]");
-    logError(error);
-  } finally {
-    const end = performance.now();
-    const time = Math.round((end - start) / 10) / 100;
-    logInfo("[" + name + "] scraped in " + time + " seconds");
-  }
+  logInfo("Writing [" + name + "] to database...");
+  await writeDBFile(name, content);
+  logSuccess("[" + name + "] written successfully");
 }
 
 const name = process.argv.pop();
+const start = performance.now();
 
-if (SCRAPERS[name]) {
-  logInfo("Scraping [" + name + "] data from the Official Temtem Wiki...");
+try {
+  if (SCRAPERS[name]) {
+    logInfo("Scraping [" + name + "] data from the Official Temtem Wiki...");
 
-  await scrapeAndSave(name);
-} else {
-  logInfo("Scraping all data from the Official Temtem Wiki...");
+    await scrapeAndSave(name);
+  } else {
+    logInfo("Scraping all data from the Official Temtem Wiki...");
 
-  for (const infoToScrape of Object.keys(SCRAPERS)) {
-    await scrapeAndSave(infoToScrape);
+    for (const infoToScrape of Object.keys(SCRAPERS)) {
+      await scrapeAndSave(infoToScrape);
+    }
   }
+} catch (error) {
+  logError("Error scraping [" + name + "]");
+  logError(error);
+} finally {
+  const end = performance.now();
+  const time = Math.round((end - start) / 10) / 100;
+  logInfo("[" + name + "] scraped in " + time + " seconds");
 }
