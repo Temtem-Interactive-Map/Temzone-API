9021548477ecf74921723b6fc47dcd525a67abb5
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-10 09:08:41 +0100

    Update build script

--- build.js
+++ build.js
@@ -1,18 +1,13 @@
 import { build } from "esbuild";
 
-try {
-  await build({
-    bundle: true,
-    sourcemap: true,
-    minify: true,
-    external: ["__STATIC_CONTENT_MANIFEST"],
-    conditions: ["worker", "browser"],
-    entryPoints: ["src/index.js"],
-    outdir: "dist",
-    outExtension: { ".js": ".mjs" },
-    format: "esm",
-    target: "esnext",
-  });
-} catch {
-  process.exitCode = 1;
-}
+await build({
+  bundle: true,
+  minify: true,
+  external: ["__STATIC_CONTENT_MANIFEST"],
+  conditions: ["worker", "browser"],
+  entryPoints: ["src/index.js"],
+  outdir: "dist",
+  outExtension: { ".js": ".mjs" },
+  format: "esm",
+  target: "esnext",
+});
