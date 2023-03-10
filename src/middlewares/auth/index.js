00540852b78282f934d240e740096e687c286f44
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-10 09:18:24 +0100

    Update auth middleware

--- src/middlewares/auth/index.js
+++ src/middlewares/auth/index.js
@@ -1,12 +1,19 @@
-import { forbidden, unauthorized } from "api/responses";
 import { decodeProtectedHeader, importX509, jwtVerify } from "jose";
+import { forbidden, unauthorized } from "responses";
 
 export function auth(admin = false) {
   return async (ctx, next) => {
     try {
       const authorization = ctx.req.headers.get("Authorization");
       const token = authorization.replace(/Bearer\s+/i, "");
-      const payload = await getPayload(token, ctx.env);
+      const data = await getPublicKeys(ctx);
+      const header = decodeProtectedHeader(token);
+      const certificate = data[header.kid];
+      const publicKey = await importX509(certificate, "RS256");
+      const { payload } = await jwtVerify(token, publicKey, {
+        issuer: "https://securetoken.google.com/" + ctx.env.FIREBASE_PROJECT_ID,
+        audience: ctx.env.FIREBASE_PROJECT_ID,
+      });
 
       if (admin && !payload.admin) {
         return forbidden(ctx);
@@ -24,49 +31,13 @@ export function auth(admin = false) {
   };
 }
 
-async function getPayload(token, env) {
-  if (env.FIREBASE_EMULATOR) {
-    const b64Url = token.split(".")[1];
-    const b64 = b64Url.replace(/_|-/g, (m) => ({ _: "/", "-": "+" }[m] ?? m));
-    const decoded = decodeBase64(b64);
-    const utf8Decoder = new TextDecoder();
-    const payload = JSON.parse(utf8Decoder.decode(decoded));
-
-    return payload;
-  } else {
-    const data = await getPublicKeys(env.cache);
-    const header = decodeProtectedHeader(token);
-    const certificate = data[header.kid];
-    const publicKey = await importX509(certificate, "RS256");
-    const { payload } = await jwtVerify(token, publicKey, {
-      issuer: "https://securetoken.google.com/" + env.FIREBASE_PROJECT_ID,
-      audience: env.FIREBASE_PROJECT_ID,
-    });
-
-    return payload;
-  }
-}
-
-function decodeBase64(str) {
-  const binary = atob(str);
-  const bytes = new Uint8Array(new ArrayBuffer(binary.length));
-  const half = binary.length / 2;
-
-  for (let i = 0, j = binary.length - 1; i <= half; i++, j--) {
-    bytes[i] = binary.charCodeAt(i);
-    bytes[j] = binary.charCodeAt(j);
-  }
-
-  return bytes;
-}
-
-async function getPublicKeys(cache) {
-  const data = await cache.get("firebase-public-key", "json");
+async function getPublicKeys(ctx) {
+  const data = await ctx.env.CACHE.get("firebase-public-key", "json");
 
   if (data === null) {
     const { data, maxAge } = await fetchPublicKeys();
 
-    await cache.put("firebase-public-key", JSON.stringify(data), {
+    await ctx.env.CACHE.put("firebase-public-key", JSON.stringify(data), {
       expirationTtl: maxAge,
     });
 
