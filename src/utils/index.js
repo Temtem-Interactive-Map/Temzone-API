c0b4ec53f88d7ebde141587240b7540177bbe8e6
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-10 19:03:16 +0100

    Update utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,10 +1,36 @@
 import { z } from "zod";
 
-const validMarkerTypes = ["spawn", "saipark"];
+// Map properties
+const ZOOM = 6;
+const TILE_SIZE = 256;
+const MAP_SIZE = TILE_SIZE * Math.pow(2, ZOOM);
+const MAP_MIN_HORIZONTAL = TILE_SIZE * 7;
+const MAP_MAX_HORIZONTAL = MAP_SIZE - TILE_SIZE * 7;
+const MAP_MIN_VERTICAL = TILE_SIZE * 11;
+const MAP_MAX_VERTICAL = MAP_SIZE - TILE_SIZE * 11;
+
+// Marker properties
+const VALID_MARKER_TYPES = ["spawn", "saipark"];
+
+export const id = z.string().uuid();
 
 export const type = z
   .string()
-  .refine((type) => validMarkerTypes.includes(type));
+  .refine((type) => VALID_MARKER_TYPES.includes(type));
+
+export const title = z.string().max(40);
+
+export const subtitle = z.string().max(40);
+
+export const condition = z.string().max(40).nullable().default(null);
+
+export const coordinates = z
+  .object({
+    x: z.number().min(MAP_MIN_HORIZONTAL).max(MAP_MAX_HORIZONTAL),
+    y: z.number().min(MAP_MIN_VERTICAL).max(MAP_MAX_VERTICAL),
+  })
+  .nullable()
+  .default(null);
 
 export const types = z
   .preprocess((types) => {
@@ -12,7 +38,9 @@ export const types = z
 
     return [...new Set(types.split(/%2C/))];
   }, z.string().array())
-  .refine((types) => types.every((type) => validMarkerTypes.includes(type)));
+  .refine((types) => types.every((type) => VALID_MARKER_TYPES.includes(type)));
+
+export const query = z.string();
 
 export const limit = z
   .string()
