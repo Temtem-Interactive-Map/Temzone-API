f694dd33241e2d25035b1fa124165fe45ac0f34c
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-10 09:08:50 +0100

    Update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,8 +1,9 @@
+import jsconfigPaths from "vite-jsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  plugins: [jsconfigPaths()],
   test: {
     watch: false,
-    environment: "miniflare",
   },
 });
