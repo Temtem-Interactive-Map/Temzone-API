c89ed5239405ac33261ee85ef456e82e115413e0
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-17 01:09:51 +0100

    Add error logging in database populate script

--- scraper/database/index.js
+++ scraper/database/index.js
@@ -1,62 +1,69 @@
 import { config } from "dotenv";
 import { readDBFile } from "../utils/database/index.js";
-import { logInfo, logSuccess } from "../utils/log/index.js";
+import { logError, logInfo, logSuccess } from "../utils/log/index.js";
 
-config();
+try {
+  config();
 
-const markers = [];
-const spawns = await readDBFile("spawns");
-const saipark = await readDBFile("saipark");
+  const markers = [];
+  const spawns = await readDBFile("spawns");
+  const saipark = await readDBFile("saipark");
 
-Object.entries(spawns).forEach(([id, spawn]) => {
-  markers.push({
-    id,
-    type: "spawn",
-    title: spawn.title,
-    subtitle: spawn.subtitle,
+  Object.entries(spawns).forEach(([id, spawn]) => {
+    markers.push({
+      id,
+      type: "spawn",
+      title: spawn.title,
+      subtitle: spawn.subtitle,
+    });
   });
-});
 
-Object.entries(saipark).forEach(([id, saipark]) => {
-  markers.push({
-    id,
-    type: "saipark",
-    title: saipark.title,
-    subtitle: saipark.subtitle,
+  Object.entries(saipark).forEach(([id, saipark]) => {
+    markers.push({
+      id,
+      type: "saipark",
+      title: saipark.title,
+      subtitle: saipark.subtitle,
+    });
   });
-});
 
-const token = await fetch(
-  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
-    process.env.FIREBASE_API_KEY,
-  {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email: process.env.FIREBASE_USER_EMAIL,
-      password: process.env.FIREBASE_USER_PASSWORD,
-      returnSecureToken: true,
-    }),
-  }
-)
-  .then((response) => response.json())
-  .then((data) => data.idToken);
-
-let countMarkers = 0;
-while (markers.length > 0) {
-  countMarkers += await fetch(process.env.TEMZONE_BASE_URL + "/markers", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      Authorization: "Bearer " + token,
-    },
-    body: JSON.stringify(markers.splice(0, 200)),
-  })
+  const token = await fetch(
+    "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=" +
+      process.env.FIREBASE_API_KEY,
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: process.env.FIREBASE_USER_EMAIL,
+        password: process.env.FIREBASE_USER_PASSWORD,
+        returnSecureToken: true,
+      }),
+    }
+  )
     .then((response) => response.json())
-    .then((markers) => markers.length);
-}
+    .then((data) => data.idToken);
 
-logSuccess("Markers inserted successfully");
-logInfo("Inserted " + countMarkers + " markers");
+  let countMarkers = 0;
+  while (markers.length > 0) {
+    countMarkers += await fetch(process.env.TEMZONE_BASE_URL + "/markers", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + token,
+      },
+      body: JSON.stringify(markers.splice(0, 200)),
+    })
+      .then((response) => response.json())
+      .then((markers) => markers.length);
+  }
+
+  logSuccess("Markers inserted successfully");
+  logInfo("Inserted " + countMarkers + " markers");
+} catch (error) {
+  logError("Error inserting markers");
+  logError(error);
+
+  throw error;
+}
