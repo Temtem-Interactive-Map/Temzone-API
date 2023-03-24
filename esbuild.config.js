3e2cbf0d63a1552a2ec408a8c5af53e3383e2874
TheLokin <diego.ramil.lopez@gmail.com>
2023-03-24 01:46:06 +0100

    Update esbuild config

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,13 +1,22 @@
+import { sentryEsbuildPlugin } from "@sentry/esbuild-plugin";
 import { build } from "esbuild";
 
 await build({
   bundle: true,
   minify: true,
+  sourcemap: true,
   external: ["__STATIC_CONTENT_MANIFEST"],
-  conditions: ["worker", "browser"],
   entryPoints: ["src/index.ts"],
   outdir: "dist",
   outExtension: { ".js": ".mjs" },
   format: "esm",
   target: "esnext",
+  plugins: [
+    sentryEsbuildPlugin({
+      org: process.env.SENTRY_ORG,
+      project: process.env.SENTRY_PROJECT,
+      authToken: process.env.SENTRY_AUTH_TOKEN,
+      include: "dist",
+    }),
+  ],
 });
