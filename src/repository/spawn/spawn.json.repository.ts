81471c6cc6132f8821df5ae49ade6b9d0deba0ce
TheLokin <diego.ramil.lopez@gmail.com>
2023-04-12 21:45:53 +0200

    Update spawn repository

--- src/repository/spawn/spawn.json.repository.ts
+++ src/repository/spawn/spawn.json.repository.ts
@@ -1,15 +1,21 @@
-import spawns from "database/spawns.json";
+import spawnsJson from "database/spawns.json";
 import { SpawnEntity } from "repository/spawn/model/spawn.entity";
 import { SpawnRepository } from "repository/spawn/spawn.repository";
 
 export class SpawnJsonRepository implements SpawnRepository {
   findById(id: string): SpawnEntity {
-    const marker = (spawns as Record<string, SpawnEntity>)[id];
+    const spawn = (spawnsJson as Record<string, unknown>)[id];
 
-    if (!marker) {
+    if (!spawn) {
       throw new Error("Spawn with id " + id + " not found");
     }
 
-    return marker;
+    return { id, ...spawn } as SpawnEntity;
+  }
+
+  getByTemtemId(temtemId: string): SpawnEntity[] {
+    return Object.entries(spawnsJson)
+      .filter(([, spawn]) => spawn.temtemId === temtemId)
+      .map(([id, spawn]) => ({ id, ...spawn } as SpawnEntity));
   }
 }
