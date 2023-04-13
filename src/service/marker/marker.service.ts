3b2ac87644c35475343320e4c1baf17167d74db3
TheLokin <diego.ramil.lopez@gmail.com>
2023-04-13 02:13:31 +0200

    Update marker service

--- src/service/marker/marker.service.ts
+++ src/service/marker/marker.service.ts
@@ -1,19 +1,20 @@
 import { Page } from "model/page";
 import { Marker } from "service/marker/model/marker";
 import { SaiparkMarker } from "service/marker/model/saipark.marker";
+import { SaiparkMarkerDetails } from "service/marker/model/saipark.marker.details";
 import { SpawnMarker } from "service/marker/model/spawn.marker";
-import { UserMarker } from "service/marker/model/user.marker";
+import { SpawnMarkerDetails } from "service/marker/model/spawn.marker.details";
 
 export interface MarkerService {
-  insertMany(markers: Marker[]): Promise<Marker[]>;
+  insertMarkers(markers: Marker[]): Promise<Marker[]>;
+  getSpawnMarker(id: string, baseUrl: string): Promise<SpawnMarkerDetails>;
   updateSpawnMarker(id: string, spawn: SpawnMarker): Promise<void>;
+  getSaiparkMarker(id: string, baseUrl: string): Promise<SaiparkMarkerDetails>;
   updateSaiparkMarker(id: string, saipark: SaiparkMarker): Promise<void>;
-  markTemtemObtained(userId: string, temtemId: string): Promise<void>;
-  search(query: string, limit: number, offset: number): Promise<Page<Marker>>;
-  getPage(limit: number, offset: number): Promise<Page<Marker>>;
-  getUserPage(
-    userId: string,
+  getMarkers(limit: number, offset: number): Promise<Page<Marker>>;
+  searchMarkers(
+    query: string,
     limit: number,
     offset: number
-  ): Promise<Page<UserMarker>>;
+  ): Promise<Page<Marker>>;
 }
