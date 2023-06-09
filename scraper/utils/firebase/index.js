61de9e884c356af3470f932d846682313a4990c4
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-09 20:36:47 +0200

    Add firebase messaging config

---
+++ scraper/utils/firebase/index.js
@@ -0,0 +1,14 @@
+import { config } from "dotenv";
+import admin from "firebase-admin";
+
+config();
+
+admin.initializeApp({
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY,
+  }),
+});
+
+export const messaging = admin.messaging(admin.app());
