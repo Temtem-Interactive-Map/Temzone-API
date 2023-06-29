d4f9c69f9a04a0b0cf86ef57f3bf9d5a94fc8ff4
TheLokin <diego.ramil.lopez@gmail.com>
2023-06-29 07:39:36 +0200

    Update user service

--- src/config/service/user.service.ts
+++ src/config/service/user.service.ts
@@ -2,6 +2,7 @@ import { initSqliteDatabase } from "config/repository/database/kysely.database";
 import { getMarkerUserRepository } from "config/repository/marker-user.repository";
 import { getMarkerRepository } from "config/repository/marker.repository";
 import { getSpawnRepository } from "config/repository/spawn.repository";
+import { getTemtemRepository } from "config/repository/temtem.repository";
 import { Context } from "hono";
 import { UserService } from "service/user/user.service";
 import { UserServiceImpl } from "service/user/user.service.impl";
@@ -12,10 +13,12 @@ export function getUserService(ctx: Context): UserService {
   const markerRepository = getMarkerRepository();
   const markerUserRepository = getMarkerUserRepository();
   const spawnRepository = getSpawnRepository();
+  const temtemRepository = getTemtemRepository();
   const userService = new UserServiceImpl(
     markerRepository,
     markerUserRepository,
-    spawnRepository
+    spawnRepository,
+    temtemRepository
   );
 
   return userService;
